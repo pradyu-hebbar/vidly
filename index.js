97853ed565b64edd6c682b4cb2d60960ca0bd5ba
Pradyumna Hebbar <pradyu.hebbar@gmail.com>
2021-05-11 12:10:51 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,75 @@
+const Joi = require("joi");
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+const genres = [
+  {
+    id: 1,
+    type: "Horror",
+  },
+  {
+    id: 2,
+    type: "Drama",
+  },
+  {
+    id: 3,
+    type: "Action",
+  },
+];
+
+app.get("/api/genres", (req, res) => {
+  res.send(genres);
+});
+
+app.post("/api/genres", (req, res) => {
+  const { error } = validateGenre(req.body);
+
+  if (error) return res.status(400).send(error.details[0].message);
+
+  const genre = {
+    id: genres.length + 1,
+    name: req.body.name,
+  };
+  genres.push(genre);
+  res.send(genres);
+});
+
+app.put("/api/genres/:id", (req, res) => {
+  const genre = genres.find((g) => g.id === parseInt(req.params.id));
+  if (!genre) return res.status(404).send("This genre is not available");
+  const { error } = validateGenre(req.body);
+
+  if (error) return res.status(400).send(error.details[0].message);
+  genre.name = req.body.name;
+  res.send(genre);
+});
+
+app.delete("/api/genres/:id", (req, res) => {
+  const genre = genres.find((g) => g.id === parseInt(req.params.id));
+  if (!genre) return res.status(404).send("This genre is not available");
+
+  const index = genres.indexOf(genre);
+
+  genres.splice(index, 1);
+
+  res.send(genre);
+});
+
+app.get("/api/genres/:id", (req, res) => {
+  const genre = genres.find((g) => g.id === parseInt(req.params.id));
+  if (!genre) return res.status(404).send("This genre is not available");
+  res.send(genre);
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Litsening on port ${port}`));
+
+function validateGenre(genre) {
+  const schema = {
+    name: Joi.string().min(3).required(),
+  };
+
+  return Joi.validate(genre, schema);
+}
